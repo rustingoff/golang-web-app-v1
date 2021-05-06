b8a3629a3148bb6655385914100332dfa86548af
rustingoff <nailunho@gmail.com>
2021-05-06 22:46:54 +0300

    Login added

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"github.com/go-redis/redis/v7"
-	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
+
+	"github.com/go-redis/redis/v7"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/sessions"
 )
 
 var client *redis.Client
+var store = sessions.NewCookieStore([]byte("t0p-s3cr3t"))
 var templates *template.Template
 
 func main() {
@@ -16,12 +19,17 @@ func main() {
 	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/", IndexHandler).Methods("GET")
+	r.HandleFunc("/", indexGetHandler).Methods("GET")
+	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	r.HandleFunc("/login", loginGetHandler).Methods("GET")
+	r.HandleFunc("/login", loginPostHandler).Methods("POST")
+	fs := http.FileServer(http.Dir("./static/"))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
 
-func IndexHandler (w http.ResponseWriter, r *http.Request){
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
 	comments, err := client.LRange("comments", 0, 10).Result()
 	if err != nil {
 		return
@@ -29,3 +37,21 @@ func IndexHandler (w http.ResponseWriter, r *http.Request){
 	templates.ExecuteTemplate(w, "index.html", comments)
 }
 
+func indexPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	comment := r.PostForm.Get("comment")
+	client.LPush("comments", comment)
+	http.Redirect(w, r, "/", 302)
+}
+
+func loginGetHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "login.html", nil)
+}
+
+func loginPostHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	username := r.PostForm.Get("username")
+	session, _ := store.Get(r, "session")
+	session.Values["username"] = username
+	session.Save(r, w)
+}
--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/go-redis/redis/v7 v7.4.0 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/sessions v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
 )
