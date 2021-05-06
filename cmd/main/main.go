b0a30c20043498976b7200b5dbaab6e04e0d1d3e
Alz Heimer <nailunho@gmail.com>
2021-05-06 20:01:01 +0300

    Mux pkg

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handler (w http.ResponseWriter, r *http.Request){
+func hellohandler (w http.ResponseWriter, r *http.Request){
 	fmt.Fprint(w, "Hello World")
 }
+
+func goodbyehandler(w http.ResponseWriter, r *http.Request)  {
+	fmt.Fprint(w, "GoodBye")
+}
+
 func main() {
-	http.HandleFunc("/", handler)
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", hellohandler).Methods("GET")
+	r.HandleFunc("/goodbye", goodbyehandler).Methods("GET")
+	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
