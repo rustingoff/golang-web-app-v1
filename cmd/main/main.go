80941093513257d741734431442eeb59abc9b6a0
Alz Heimer <nailunho@gmail.com>
2021-05-06 20:10:07 +0300

    Using templates

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,23 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/gorilla/mux"
+	"html/template"
 	"net/http"
 )
 
-func hellohandler (w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "Hello World")
-}
-
-func goodbyehandler(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprint(w, "GoodBye")
-}
+var templates *template.Template
 
 func main() {
+	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
-	r.HandleFunc("/hello", hellohandler).Methods("GET")
-	r.HandleFunc("/goodbye", goodbyehandler).Methods("GET")
+	r.HandleFunc("/", IndexHandler).Methods("GET")
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
 }
+
+func IndexHandler (w http.ResponseWriter, r *http.Request){
+	templates.ExecuteTemplate(w, "index.html", nil)
+}
+
