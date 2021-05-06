24ac70c61556bc29413a7893669439acaae9c352
rustingoff <nailunho@gmail.com>
2021-05-06 22:02:33 +0300

    redis connect

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
+	"github.com/go-redis/redis/v7"
 	"github.com/gorilla/mux"
 	"html/template"
 	"net/http"
 )
 
+var client *redis.Client
 var templates *template.Template
 
 func main() {
+	client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 	r := mux.NewRouter()
 	r.HandleFunc("/", IndexHandler).Methods("GET")
@@ -17,6 +22,10 @@ func main() {
 }
 
 func IndexHandler (w http.ResponseWriter, r *http.Request){
-	templates.ExecuteTemplate(w, "index.html", nil)
+	comments, err := client.LRange("comments", 0, 10).Result()
+	if err != nil {
+		return
+	}
+	templates.ExecuteTemplate(w, "index.html", comments)
 }
 
